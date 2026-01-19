e93b57bb74f8545019c256f83c81d65731272ec9
Lucasgarcc <dev2@f5sg.com.br>
2026-01-19 20:56:28 -0300

    feat: feat: :sparkles: adiciona componentes essenciais ao `Login`

---
+++ src/components/Login/LoginPasswordReset/LoginPasswordReset.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './LoginPasswordReset.module.css';
+
+const LoginPasswordReset = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LoginPasswordReset
