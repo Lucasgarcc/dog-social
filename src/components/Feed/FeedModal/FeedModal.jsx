a77d9c3730a3933990819e18b29cd7d45e122a8a
Lucasgarcc <dev2@f5sg.com.br>
2026-02-18 16:29:15 -0300

    feat: :sparkles: adiciona modal de fotos, ajusta api para routes e cria componentes photos

--- src/components/Feed/FeedModal/FeedModal.jsx
+++ src/components/Feed/FeedModal/FeedModal.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import styles from './FeedModal.module.css'
+import React from 'react';
+import styles from './FeedModal.module.css';
+import useFetch from '../../../hooks/useFetch/useFetch';
+import { PHOTO_GET } from '../../../routes/endpoints/endpoints';
+import Error from '../../Helpers/Error/Error';
+import Loading from '../../Helpers/Loading/Loading';
+import PhotoContent from '../../ui/Photo/PhotoContent/PhotoContent';
 
-const FeedModal = () => {
+const FeedModal = ({ photo, setModalPhoto}) => {
     
-    return (
-        <div>
+    const { data, loading, error, request } = useFetch();
 
+    React.useEffect(() =>{
+
+        const { url, options } = PHOTO_GET(photo.id);
+        request(url, options);
+        
+    },[photo, request]);
+
+    const handleOutsideClick = (e) => {
+
+        if (e.target === e.currentTarget) {
+            setModalPhoto(null);
+        }
+    }
+
+    return (
+    
+        <div className={styles.modal} onClick={handleOutsideClick}>
+
+            {error && 
+                <Error error={error} />
+            }
+
+            {loading &&
+                <Loading />
+            }
+
+            {data &&  
+                <PhotoContent data={data} />
+            }
         </div>
+    
     )
 }
 
