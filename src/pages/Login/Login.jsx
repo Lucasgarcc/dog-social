75f9cec0d3620137bc677e614a5e692347d94c21
Lucasgarcc <dev2@f5sg.com.br>
2026-02-24 15:52:16 -0300

    feat: :sparkles: adiciona componente de redefinição de senha do usuário

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
 						element={<LoginPasswordLost />}
 					/>
 					<Route
-						path="/reset"
+						path="/resetar"
 						element={<LoginPasswordReset />}
 					/>
 					<Route
